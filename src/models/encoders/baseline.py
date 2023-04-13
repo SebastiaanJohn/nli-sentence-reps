800bc3afd9fa2a4e7d61946233d29c9c88e40aa7
Sebastiaan Dijkstra <sebastiaandijkstra@gmail.com>
2023-04-13 15:38:22 +0200

    Updated baseline

--- src/models/encoders/baseline.py
+++ src/models/encoders/baseline.py
@@ -1,20 +1,41 @@
 """Baseline sentence encoder: averaging word embeddings to obtain sentence representations."""
 
 import torch
+from torch import nn
 
 
-class BaselineEncoder(torch.nn.Module):
+class BaselineEncoder(nn.Module):
     """Baseline sentence encoder: averaging word embeddings to obtain sentence representations."""
-
-    def forward(self, sentence: torch.Tensor) -> torch.Tensor:
-        """Compute sentence embeddings for a batch of sentences.
+    def __init__(self, aligned_embeddings: torch.Tensor) -> None:
+        """Initialize the encoder.
 
         Args:
-            sentence: Tensor of shape (batch_size, max_sentence_length, embedding_size) containing
-                word embeddings.
-
-        Returns:
-            Tensor of shape (batch_size, embedding_size) containing sentence embeddings.
+            aligned_embeddings (torch.Tensor): Aligned word embeddings (vocab_size, embedding_dim).
         """
-        return sentence.mean(dim=1)
+        super(BaselineEncoder, self).__init__()
+        self.embedding = nn.Embedding.from_pretrained(aligned_embeddings, freeze=True)
+
+    def forward(self, token_indices: torch.Tensor) -> torch.Tensor:
+        """Compute sentence representations by averaging word embeddings.
+
+        Args:
+            token_indices (torch.Tensor): Tensor of token indices of shape (batch_size, sequence_length).
+        """
+        # Convert token indices to embeddings
+        embeddings = self.embedding(token_indices)
+
+        # Compute the mask to ignore padding tokens in the averaging process
+        mask = token_indices.ne(0).float().unsqueeze(-1)
+
+        # Compute the sum of embeddings along the sequence dimension
+        sum_embeddings = (embeddings * mask).sum(dim=1)
+
+        # Compute the number of non-padding tokens in each sequence
+        num_tokens = mask.sum(dim=1)
+
+        # Average the embeddings
+        averaged_embeddings = sum_embeddings / num_tokens
+
+        return averaged_embeddings
+
 
